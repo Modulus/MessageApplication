4db3eae1e9d9acc1a2e17aef6f5a860f0c82b701
Modulus <thejohnskauge@gmail.com>
2014-11-06 15:16:05 +0100

    sending static file at root

--- MessageApplication.py
+++ MessageApplication.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='webapp/dist', static_url_path='')
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def serve_root():
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
