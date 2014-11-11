ec2169d18c9e8a1d245344f3a0a74cd36dba51e8
Modulus <thejohnskauge@gmail.com>
2014-11-11 13:06:03 +0100

    Jaddam

---
+++ tests/models/message_test.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+from mongoengine import ValidationError
+from app.models.Message import Message
+from app.models.User import User
+
+__author__ = 'johska'
+
+
+class MessageTest(TestCase):
+
+    def test_missing_message_raise_validation_error(self):
+
+        with self.assertRaises(ValidationError):
+            message = Message()
+            message.validate()
+
+    def test_message_fields(self):
+        message = Message()
+
+        self.assertTrue(hasattr(message, 'header'))
+        self.assertTrue(hasattr(message, 'message'))
+        self.assertTrue(hasattr(message, 'receivers'))
+        self.assertTrue(hasattr(message, 'sender'))
+"""
+    def test_valid_message(self):
+        users = []
+        users.append(User(name='Nils', hash='myHash', salt='mySalt'))
+        users.append(User(name='Stine', hash='myHash2', salt='mySalt2'))
+        users.append(User(name='Geir', hash='myHash3', salt='mySalt3'))
+
+        message_text = 'Hallaisen!'
+        sender = User(name='Nils', hash='myHash', salt='mySalt')
+
+        message = Message(message=message_text, receivers=users, sender=sender)
+        message.validate()
+"""
---
+++ tests/models/user_test.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from unittest import TestCase
+from mongoengine import ValidationError, StringField
+from app.models.User import User
+
+__author__ = 'johska'
+
+
+class UserTest(TestCase):
+
+    def test_user_fields(self):
+        user = User(name='Nils', hash='myhash', salt='1213213')
+
+        self.assertTrue(hasattr(user, 'name'))
+        self.assertEquals(str, type(user.name))
+
+        self.assertTrue(hasattr(user, 'hash'))
+        self.assertEquals(str, type(user.hash))
+
+        self.assertTrue(hasattr(user, 'salt'))
+        self.assertEquals(str, type(user.salt))
+
+        self.assertTrue(hasattr(user, 'created'))
+        self.assertEquals(datetime, type(user.created))
+
+    def test_user_missing_fields_error(self):
+        with self.assertRaises(ValidationError):
+            user = User()
+            user.validate()
+
+    def test_valid_user(self):
+        user = User(name='Nils', hash='myhash', salt='1213213')
+        user.validate()
+        self.assertIsNotNone(user)
